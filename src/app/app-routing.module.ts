8bb39be5ba80bcb802c43d27071fb3c4c0868753
Victor Calfiman <vcalfiman1@gmail.com>
2025-06-15 12:10:43 -0400

    validacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './auth.guard';
 import { TareasComponent } from './tareas/tareas.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'tareas', component: TareasComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'tareas', component: TareasComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
